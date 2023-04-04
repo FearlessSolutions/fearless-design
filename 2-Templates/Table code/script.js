501f65c2ecfe0fc44d38c652935be05fcf8658a5
Virginia Peters-Rodbell <127786337+vpeters-Rodbell@users.noreply.github.com>
2023-04-04 15:03:09 -0400

    Update script.js

--- 2-Templates/Table code/script.js
+++ 2-Templates/Table code/script.js
@@ -1,14 +1,30 @@
-const calculateBtn = document.getElementById("calculate-btn");
+// Get all the "Number of Confirmed" elements
+const confirmedElems = document.querySelectorAll('td:nth-of-type(4)');
 
-calculateBtn.addEventListener("click", () => {
-  const adultsInput = document.getElementById("adults");
-  const childrenInput = document.getElementById("children");
-  const totalInput = document.getElementById("total");
+// Get all the "No Response" elements
+const noResponseElems = document.querySelectorAll('td:nth-of-type(5)');
 
-  const adults = parseInt(adultsInput.value) || 0;
-  const children = parseInt(childrenInput.value) || 0;
+// Get all the "Unable to Attend" elements
+const unableToAttendElems = document.querySelectorAll('td:nth-of-type(6)');
 
-  const total = adults + children;
+// Initialize totals to 0
+let confirmedTotal = 0;
+let noResponseTotal = 0;
+let unableToAttendTotal = 0;
 
-  totalInput.value = total.toString();
-});
+// Loop through each row of the table
+for (let i = 0; i < confirmedElems.length; i++) {
+	// Add the confirmed value to the confirmed total
+	confirmedTotal += parseInt(confirmedElems[i].textContent);
+
+	// Add the no response value to the no response total
+	noResponseTotal += parseInt(noResponseElems[i].textContent);
+
+	// Add the unable to attend value to the unable to attend total
+	unableToAttendTotal += parseInt(unableToAttendElems[i].textContent);
+}
+
+// Set the total values in the table footer
+document.getElementById('confirmed-total').textContent = confirmedTotal;
+document.getElementById('no-response-total').textContent = noResponseTotal;
+document.getElementById('unable-to-attend-total').textContent = unableToAttendTotal;
